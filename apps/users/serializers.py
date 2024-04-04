a156cb9736e090d6d4b783c2a9b9bc5fd776e452
mohammad <mmdhosein.yaghoubi@gmail.com>
2024-04-04 23:33:50 +0330

    add: custom TokenObtainPairView & register api

---
+++ apps/users/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["username"] = user.username
+        # token['email'] = user.email
+        return token
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username",)
+
+
+class UserRegSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    # email = serializers.EmailField()
+    password = serializers.CharField(max_length=128, write_only=True)
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from apps.users.models import User
+from .serializers import UserRegSerializer, MyTokenObtainPairSerializer
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+@api_view(["POST"])
+def user_registration(request):
+    serializer = UserRegSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        username = serializer.validated_data.get("username")
+        password = serializer.validated_data.get("password")
+
+        if User.objects.filter(username=username).exists():
+            return Response(
+                {"detail": "username is already exist."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        user = User.objects.create_user(username=username, password=password)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
